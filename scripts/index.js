eec1b861832e6b2cc072591c6a0820d0dccf98ae
brandi.jeff <brandi.jeff2@gmail.com>
2023-02-09 11:21:31 -0500

    updated css on those needed. linked add to cart buttons to cart

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
-// to be fleshed out
+const city = "Orlando";
+const url = `http://api.weatherapi.com/v1/forecast.json?key=a33dec18f1bd45bda0931106230802&q=${city}&days=10`;
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const getCurrentWeather = data.current.temp_f;
+    const getCurrentCondition = data.current.condition.text;
+    const currentWeather = `Current Weather: ${getCurrentWeather}\u00B0C, Current Condition: ${getCurrentCondition}`;
+    document.getElementById("current-weather").innerText = currentWeather;
+  });
